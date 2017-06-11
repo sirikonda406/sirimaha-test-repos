5fe629fc536b1c4f2907ecd5aa2e61bfe773ab9e
sirikonda406 <mahesh.sirikonda@gmail.com>
2017-06-11 21:52:48 +0200

    first checkin

---
+++ src/main/java/com/mahesh/test/GitHubTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mahesh.test;
+
+/**
+ * @author maheshsirikonda
+ *
+ */
+public class GitHubTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hi git");
+
+	}
+
+}
