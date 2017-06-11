e78e7e255e80d988ee06fb0a033fafb479972dc8
sirikonda406 <mahesh.sirikonda@gmail.com>
2017-06-11 10:51:56 +0200

    test again

--- src/main/java/com/test/mahesh/GitTest.java
+++ src/main/java/com/test/mahesh/GitTest.java
@@ -14,7 +14,7 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		System.out.println("git test branch");
-		System.out.println("git test");
+		System.out.println("git test branch git");
 
 	}
 
